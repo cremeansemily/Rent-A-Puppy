bfd0f5884ee4d2baee976d87f304bdb818116592
iiTONELOC <anthonytropeano@me.com>
2021-05-28 20:09:57 -0400

    added a file preview, added tailwinds css to the main handlebars with tailwinds html template, link for our stylesheet, test page for uploading and previewing images

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
-
+const uploadRoute = require ('./upload-routes');
 // TURN ON LATER AFTER TESTING
 // const dashboardRoutes = require('./dashboard-routes');
 
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/upload-test', uploadRoute);
 // TURN ON LATER FOR HOME PAGE AND USER DASHBOARD
 
 // router.use('/dashboard', dashboardRoutes);
