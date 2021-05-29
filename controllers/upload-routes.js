be7b1271b5fd37d3007512f627a68d8cd3296e89
iiTONELOC <anthonytropeano@me.com>
2021-05-29 06:16:03 -0400

    removed auth for now

--- controllers/upload-routes.js
+++ controllers/upload-routes.js
@@ -3,7 +3,7 @@ const withAuth = require('../utils/auth');
 const router = require('express').Router();
 
 
-router.get('/', withAuth, (req, res) => {
+router.get('/',  (req, res) => {
     console.log(req.session.user_id)
     res.render('upload')
 
