3178ba4563936dc35561d8b0b636115659265528
Mona Lisa <isaac11195@gmail.com>
2021-05-31 17:24:35 -0400

    sorry make a mistake

--- auth.js
+++ auth.js
@@ -1,7 +1,11 @@
 const withAuth = (req, res, next) => {
     console.log(req.session.user_id)
     if (!req.session.user_id) {
+<<<<<<< HEAD
         res.redirect('/user/login');
+=======
+        res.redirect('/login');
+>>>>>>> feature/review
         
     } else {
         next();
