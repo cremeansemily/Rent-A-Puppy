94954c8240c32b1e33f4b8e7806b241002587c4c
iiTONELOC <anthonytropeano@me.com>
2021-05-31 21:07:01 -0400

    added a user home route

--- controllers/user-routes.js
+++ controllers/user-routes.js
@@ -1,18 +1,44 @@
 const router = require('express').Router();
+const FetchData = require('../utils/api/fetches');
 
 
 // login page-user
 router.get('/login', async (req, res) => {
-    if(req.session.user_id){
+    if (req.session.user_id) {
         return res.redirect(`/dashboard/user/${req.session.user_id}`);
-    }else{
-    return res.render('user-views/login')
+    } else {
+        return res.render('user-views/login');
     }
-    
+
 });
 // Signup page-user
 router.get('/signup', (req, res) => {
-    return res.render('user-views/sign-up')
+    return res.render('user-views/sign-up');
 });
+
+// user home
+router.get('/home', async (req, res) => {
+
+    try {
+        const fetch = await FetchData.allPets();
+        if (fetch === null) {
+            return res.redirect('/error');
+        } else {
+            const data = {
+                pet: fetch,
+                loggedIn: req.session.loggedIn,
+                user: req.session.username
+            }
+            console.log(fetch)
+            return res.render('user-views/home', data);
+        }
+
+    } catch (err) {
+        return console.log('An error occurred hitting the pet-dashboard route', err);
+    }
+
+});
+
+
 
 module.exports = router;
