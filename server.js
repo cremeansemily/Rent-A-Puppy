4923ac7577a4e84ff4b2a0a57b62295b7a297bae
iiTONELOC <anthonytropeano@me.com>
2021-06-01 02:42:03 -0400

    seeing if I can remove the same site waring on heroku

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
     secret: process.env.SESSION_SECRET,
-    cookie: { maxAge: 300000 },
+    cookie: {
+        maxAge: 300000,
+        sameSite: 'Strict',
+        secure: false
+    },
     resave: false,
     saveUninitialized: true,
     rolling: false,
@@ -21,6 +25,11 @@ const sess = {
         db: sequelize
     })
 };
+
+if (process.env.PORT) {
+    app.set('trust proxy', 1)
+    sess.cookie.secure = true
+}
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
