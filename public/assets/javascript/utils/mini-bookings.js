b4eb8b88b7e65a24b5ae34d833e0a84635451540
iiTONELOC <anthonytropeano@me.com>
2021-06-10 13:32:05 -0400

    resloved issue with booked dates not showing up on the calendar

--- public/assets/javascript/utils/mini-bookings.js
+++ public/assets/javascript/utils/mini-bookings.js
@@ -1,14 +1,6 @@
 
 
-// wait for dom to load
-function ready(callbackFunction) {
-    if (document.readyState != 'loading')
-        callbackFunction()
-    else
-        document.addEventListener("DOMContentLoaded", callbackFunction)
-    return
-}
-ready(async () => {
+async function booking() {
     const host = window.location.hostname;
     const path = window.location.pathname;
     let url = 'https://pimp-my-puppy.herokuapp.com/api' + path;
@@ -30,31 +22,43 @@ ready(async () => {
 
 
     const bookings = data.bookings.forEach(el => {
-       
+       console.log(el)
         let month = el.date.split('-')[1];
         const currentMonth =  moment(new Date()).format('M');
        
         let currentDay = el.date.split('-')[2];
 
-        if (currentDay[0] == 0 || month[0] ==[0]) {
+        if (currentDay[0] == 0) {
             currentDay = currentDay[1];
+           
+        } if( month[0] ==[0]){
             month = month[1]
         }
-       
+        console.log(currentDay)
         if(month === currentMonth){
             console.log("CURRENT")
-            const day = document.getElementById(`mini-date-${currentDay}`);
-            day.setAttribute('class', " this booked p-1 flex items-end justify-center  bg-indigo-500 text-white rounded-full line-through self-end");
-            day.removeAttribute('data-booked');
-            day.setAttribute('data-booked', 'true');
+            const day = document.getElementById('mini-date-'+currentDay);
+            if(day != null){
+                console.log(day)
+                day.setAttribute('class', " this booked p-1 flex items-end justify-center  bg-indigo-500 text-white rounded-full line-through self-end");
+                day.removeAttribute('data-booked');
+                day.setAttribute('data-booked', 'true');
+            }else{
+                console.log(currentDay)
+                console.log(document.querySelectorAll("#mini-date-"+ currentDay))
+            }
+           
         }else{
-        
+            
         }
       
         return
     })
     addBooking(bookings);
-})
+}
+
+    booking()
+
 
 
 
